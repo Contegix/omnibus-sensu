b08d2043ee44cc3b0e46456bfe24131e89746b7d
rmc3 <rich.m.chatterton@gmail.com>
2014-10-07 15:54:57 -0500

    Updated libedit.rb from upstream

--- config/software/libedit.rb
+++ config/software/libedit.rb
@@ -1,12 +1,11 @@
 #
-# Copyright:: Copyright (c) 2012 Opscode, Inc.
-# License:: Apache License, Version 2.0
+# Copyright 2012-2014 Chef Software, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,43 +15,35 @@
 #
 
 name "libedit"
-version "20120601-3.0"
+default_version "20120601-3.0"
 
 dependency "ncurses"
 dependency "libgcc"
 
-source :url => "http://www.thrysoee.dk/editline/libedit-20120601-3.0.tar.gz",
-       :md5 => "e50f6a7afb4de00c81650f7b1a0f5aea"
+version "20120601-3.0" do
+  source md5: "e50f6a7afb4de00c81650f7b1a0f5aea"
+end
 
-relative_path "libedit-20120601-3.0"
+version "20130712-3.1" do
+  source md5: "0891336c697362727a1fa7e60c5cb96c"
+end
 
-env = case platform
-      when "aix"
-        {
-          "LDFLAGS" => "-maix64 -L#{install_dir}/embedded/lib -Wl,-blibpath:#{install_dir}/embedded/lib:/usr/lib:/lib",
-          "CFLAGS" => "-maix64 -I#{install_dir}/embedded/include",
-          "LD" => "ld -b64",
-          "OBJECT_MODE" => "64",
-          "ARFLAGS" => "-X64 cru"
-        }
-      else
-        {
-          "LDFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include -I#{install_dir}/embedded/include/ncurses",
-          "CFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include -I#{install_dir}/embedded/include/ncurses",
-          "LD_RUN_PATH" => "#{install_dir}/embedded/lib",
-          "LD_OPTIONS" => "-R#{install_dir}/embedded/lib"
-        }
-      end
+source url: "http://www.thrysoee.dk/editline/libedit-#{version}.tar.gz"
+
+relative_path "libedit-#{version}"
 
 build do
+  env = with_standard_compiler_flags(with_embedded_path)
+
   # The patch is from the FreeBSD ports tree and is for GCC compatibility.
   # http://svnweb.freebsd.org/ports/head/devel/libedit/files/patch-vi.c?annotate=300896
-  if platform == "freebsd"
-    patch :source => "freebsd-vi-fix.patch"
+  if freebsd?
+    patch source: "freebsd-vi-fix.patch"
   end
-  command ["./configure",
-           "--prefix=#{install_dir}/embedded"
-           ].join(" "), :env => env
-  command "make -j #{max_build_jobs}", :env => env
-  command "make -j #{max_build_jobs} install"
+
+  command "./configure" \
+          " --prefix=#{install_dir}/embedded", env: env
+
+  make "-j #{workers}", env: env
+  make "-j #{workers} install", env: env
 end
