cd881c6052246dd42e1be650984ca460aac36df6
rmc3 <rich.m.chatterton@gmail.com>
2014-10-07 15:26:20 -0500

    Updated libxml2.rb from upstream

--- config/software/libxml2.rb
+++ config/software/libxml2.rb
@@ -1,12 +1,11 @@
 #
-# Copyright:: Copyright (c) 2012 Opscode, Inc.
-# License:: Apache License, Version 2.0
+# Copyright 2012-2014 Chef Software, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,28 +15,34 @@
 #
 
 name "libxml2"
-version "2.7.8"
+default_version "2.9.1"
 
 dependency "zlib"
 dependency "libiconv"
+dependency "liblzma"
 
-source :url => "ftp://xmlsoft.org/libxml2/libxml2-#{version}.tar.gz",
-       :md5 => "8127a65e8c3b08856093099b52599c86"
+version "2.7.8" do
+  source md5: "8127a65e8c3b08856093099b52599c86"
+end
+
+version "2.9.1" do
+  source md5: "9c0cfef285d5c4a5c80d00904ddab380"
+end
+
+source url: "ftp://xmlsoft.org/libxml2/libxml2-#{version}.tar.gz"
 
 relative_path "libxml2-#{version}"
 
 build do
-  cmd = ["./configure",
-         "--prefix=#{install_dir}/embedded",
-         "--with-zlib=#{install_dir}/embedded",
-         "--with-iconv=#{install_dir}/embedded",
-         "--without-python"].join(" ")
-  env = {
-    "LDFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
-    "CFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
-    "LD_RUN_PATH" => "#{install_dir}/embedded/lib"
-  }
-  command cmd, :env => env
-  command "make -j #{max_build_jobs}", :env => {"LD_RUN_PATH" => "#{install_dir}/embedded/lib"}
-  command "make install", :env => {"LD_RUN_PATH" => "#{install_dir}/embedded/lib"}
+  env = with_standard_compiler_flags(with_embedded_path)
+
+  command "./configure" \
+          " --prefix=#{install_dir}/embedded" \
+          " --with-zlib=#{install_dir}/embedded" \
+          " --with-iconv=#{install_dir}/embedded" \
+          " --without-python" \
+          " --without-icu", env: env
+
+  make "-j #{workers}", env: env
+  make "install", env: env
 end
